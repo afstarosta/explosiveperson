911c91275a3261bf4238a978269fde3f685bdc9e
Andre Starosta <a.starosta98@gmail.com>
2021-03-27 14:30:29 -0300

    add player explode-through

--- Bomb.gd
+++ Bomb.gd
@@ -22,9 +22,17 @@ func _physics_process(delta):
 
 func explode():
     for raycast in raycasts:
-        var colliding = raycast.get_collider()
-        if(colliding and colliding.has_method("hit")):
-            colliding.hit()
+        while raycast.is_colliding():
+            var colliding = raycast.get_collider()
+            if(colliding and colliding.has_method("hit")):
+                colliding.hit()
+            
+            if(colliding.is_in_group("wall")):
+                raycast.enabled = false
+                break
+                
+            raycast.add_exception(colliding)
+            raycast.force_raycast_update()
 
 func _on_Area2D_body_exited(_body):
     collider.set_deferred("disabled",false)
