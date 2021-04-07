cd2addae5cda0a41a3370a197447e809333e2cfb
André Starosta <a.starosta98@gmail.com>
2021-04-07 17:44:48 -0300

    remake tileset

--- scripts/Level.gd
+++ scripts/Level.gd
@@ -1,8 +1,7 @@
 extends Node2D
 
-onready var FGTilemap:TileMap = $FG
+onready var FGTilemap:TileMap = $farm_FG
 
-onready var unbreakable_wall_scene:PackedScene = load("res://scenes/tiles/UnbreakableWall.tscn")
 onready var breakable_wall_scene:PackedScene = load("res://scenes/tiles/BreakableWall.tscn")
 
 var TILE_SIZE
@@ -11,18 +10,9 @@ func _ready():
     GameSettings.TILE_SIZE = FGTilemap.get_cell_size()
     GameSettings.level = self
     
-    var cells = FGTilemap.get_used_cells_by_id(0)
-    create_unbreakable_walls(cells)
-    cells = FGTilemap.get_used_cells_by_id(1)
+    var cells = FGTilemap.get_used_cells_by_id(1)
     create_breakable_walls(cells)
 
-func create_unbreakable_walls(cells):
-    for cell in cells: 
-        var wall = unbreakable_wall_scene.instance()
-        add_child(wall)
-        wall.position = Vector2(cell.x * FGTilemap.cell_size.x, cell.y * FGTilemap.cell_size.y)
-        
-    
 func create_breakable_walls(cells):
     for cell in cells: 
         var wall = breakable_wall_scene.instance()
